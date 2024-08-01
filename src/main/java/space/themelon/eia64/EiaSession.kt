4fa4529d5bcd23c8f00b8134574ff68f63c6f91c
kumaraswamy <care.xoma@gmail.com>
2024-08-01 20:39:46 +0530

    Optimisations, refactor, fix bugs

--- src/main/java/space/themelon/eia64/EiaSession.kt
+++ src/main/java/space/themelon/eia64/EiaSession.kt
@@ -1,20 +1,16 @@
 package space.themelon.eia64
 
 import org.apache.sshd.server.ExitCallback
-import space.themelon.eia64.EiaLive.Companion
 import space.themelon.eia64.TerminalColors.BLUE
 import space.themelon.eia64.TerminalColors.BOLD
 import space.themelon.eia64.TerminalColors.RED
 import space.themelon.eia64.TerminalColors.RESET
 import space.themelon.eia64.TerminalColors.YELLOW
-import space.themelon.eia64.analysis.ParserX
 import space.themelon.eia64.io.AutoCloseExecutor
 import space.themelon.eia64.io.TerminalInput
 import space.themelon.eia64.io.TerminalOutput
 import space.themelon.eia64.runtime.Executor
-import space.themelon.eia64.syntax.Lexer
 import java.io.PrintStream
-import java.util.*
 import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.atomic.AtomicReference
 
@@ -42,35 +38,32 @@ class EiaSession(
     }
 
     private fun serve(input: TerminalInput) {
-        // we may change an Executor object, so we should use atomic reference
         val executor = AtomicReference(Executor())
-
-        // for additional checks
         val shutdown = AtomicBoolean(false)
 
-        AutoCloseExecutor {
-            output.write(MESSAGE_MAX_DURATION)
+        val doShutdown: (Int) -> Unit = {
             executor.get().shutdownEvaluator()
             shutdown.set(true)
             exitCallback?.onExit(0)
         }
 
-        fun writeShell() {
-            output.write(SHELL_STYLE)
-            //if (!lineByLine)
-                //output.write("\r\n".encodeToByteArray())
+        // now we have to override default exitProcess() behaviour, we cant let it
+        // shut down the whole server lol, when someone does exit(n) in Eia
+        Executor.EIA_SHUTDOWN = doShutdown
+
+        AutoCloseExecutor {
+            output.write(MESSAGE_MAX_DURATION)
+            doShutdown(1)
         }
+
         output.write(INTRO)
-        writeShell()
-        //output.slowAnimate = false
+        output.write(SHELL_STYLE)
 
         // Provide access to standard I/O Stream
         executor.get().apply {
             standardInput = input.input // Provide direct access to the underlying stream
             standardOutput = PrintStream(output)
         }
-
-//        val codeArray = CodeArray()
 
         val helper = CompletionHelper(
             ready = { tokens ->
@@ -86,114 +79,6 @@ class EiaSession(
                 output.write(SHELL_STYLE)
             }
         )
-
-//        fun getCodeString(): String? {
-//            val code = String(codeArray.get())
-//                // make sure to remove any control characters present
-//                .replace(Regex("\\p{Cntrl}"), "")
-//                .trim()
-//            if (code.isEmpty()) return null
-//            return code
-//        }
-
-//        fun execute() {
-//            val filteredCode = getCodeString() ?: return
-//            helper.addLine()
-//            codeArray.reset()
-//            output.write(OUTPUT_STYLE)
-//            runSafely(output) {
-//                executor.get().loadMainSource(filteredCode)
-//            }
-//            writeShell()
-//        }
-
-//        fun lex() {
-//            val filteredCode = getCodeString() ?: return
-//            codeArray.reset()
-//            output.write(OUTPUT_STYLE)
-//            output.write(10)
-//
-//            runSafely(output) {
-//                Lexer(filteredCode).tokens.forEach {
-//                    output.write(it.toString().encodeToByteArray())
-//                    output.write(10)
-//                }
-//            }
-//
-//            writeShell()
-//        }
-
-//        fun parse() {
-//            val filteredCode = getCodeString() ?: return
-//            codeArray.reset()
-//            output.write(OUTPUT_STYLE)
-//            output.write(10)
-//
-//            runSafely(output) {
-//                val tokens = Lexer(filteredCode).tokens
-//                val trees = ParserX(Executor()).parse(tokens)
-//
-//                trees.expressions.forEach {
-//                    output.write(it.toString().encodeToByteArray())
-//                    output.write(10)
-//                }
-//            }
-//
-//            writeShell()
-//        }
-
-//        while (!shutdown.get()) {
-//            val letterCode = input.read()
-//            if (letterCode == -1) {
-//                exitCallback?.onExit(0)
-//                break
-//            }
-//
-//            when (val char = letterCode.toChar()) {
-//                '\u007F' -> {
-//                    // Delete Character
-//                    if (codeArray.isNotEmpty()) {
-//                        output.write(DELETE_CODE)
-//                        codeArray.delete()
-//                    }
-//                }
-//
-//                '\u0003' -> {
-//                    // Control + C Character
-//                    // Signal to end the session
-//                    exitCallback?.onExit(0)
-//                    break
-//                }
-//
-//                '\u000C' -> {
-//                    // Control + L
-//                    // Request to lex the tokens and print them
-//                    lex()
-//                }
-//
-//                '\u0010' -> {
-//                    // Control + P
-//                    // To print the parsed nodes
-//                    parse()
-//                }
-//
-//                '\u001B' -> {
-//                    // this is a control character (Escape), we simply discard it
-//                    // to not cause problems
-//                    input.read()
-//                    input.read()
-//                }
-//
-//                else -> {
-//                    output.write(letterCode)
-//                    if (char == '\n') {
-//                        execute()
-//                        continue
-//                    }
-//                    codeArray.put(letterCode.toByte())
-//                }
-//            }
-//        }
 
         fun useLine(line: String) {
             if (line == "debug") {
@@ -242,8 +127,9 @@ class EiaSession(
     }
 
     companion object {
+        private const val EIA_VERSION = 2.1
         private val INTRO = """
-            Eia64 Dev 2.1
+            Eia64 Dev $EIA_VERSION
             Type "debug" to toggle debug mode
             
             
